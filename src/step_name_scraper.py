4e2ce3d65adc01e513ead21b75ae6ff1bbb66ee3
Cynthia C <119439581+Cynthia-R-C@users.noreply.github.com>
2025-01-13 21:05:02 -0600

    Update step_name_scraper.py
    Finished code to extract all REV product names into 1 list of strings, and all REV product serials into another list of strings; same index = same product

--- src/step_name_scraper.py
+++ src/step_name_scraper.py
@@ -8,19 +8,87 @@
 
 import requests
 from bs4 import BeautifulSoup
+import selenium
+import re
 
 
-# Making a GET request
-r = requests.get('https://www.revrobotics.com/shop-all/')     # later extend to loop through all page URLs
+# Making functions to strip the result
+#def remove_angle_brackets(str):
+    # '''string str --> string newStr
+    # Removes the angle brackets and the content enclosed in it from a string'''
+    # newStr = ""
+    # inBrackets = False
+    # for i in range(len(str)):
+    #     if str[i] != "<" and not inBrackets:
+    #         newStr += str[i]
+    #     elif str[i] == "<":   # if it has entered the brackets
+    #         inBrackets = True
+    #     elif str[i] == ">":   # if it has reached the closing brackets
+    #         inBrackets = False
+    #     else:                 # if it's just inside the brackets
+    #         pass
+    # return newStr               
+#def strip_angle_brackets(strs):
+def extract_content(things):
+    '''HTML tag object list things --> string list newThings
+    Extracts the text content of a list of HTML tag objects into a list of strings'''
+    newThings = []
+    for thing in things:
+        newThings.append(thing.text.strip())     # .text gives the string content of the tag object; .strip() removes all the extra '\n' and whitespace in it
+    return newThings
+    # '''list of strings strs --> list of strings newStrs
+    # Strips the things in angle brackets off the list of strings;
+    # made for stripping serials out - may end up using for titles too'''
+    # newStrs = []     # find_all gives a list of strings
+    # for str in strs:
+    #     newStrs.append(remove_angle_brackets(str))
+    # return newStrs
 
-# check status code for response received
-# success code - 200
-print(r)
 
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
+def get_REV_products():
+    '''nothing --> list titles, list serial numbers
+    Returns a list of product names and a list of product serial numbers of all products from REV robotics'''
+    
+    # Set URL list
+    urls = ['https://www.revrobotics.com/shop-all/?limit=100&mode=bo',
+            'https://www.revrobotics.com/shop-all/?limit=100&page=2&mode=bo',
+            'https://www.revrobotics.com/shop-all/?limit=100&page=3&mode=bo',
+            'https://www.revrobotics.com/shop-all/?limit=100&page=4&mode=bo']
 
-s = soup.find('div', class_='entry-content')
-content = soup.find_all('REV-21-2099')                          # test if a product from page 2 can be detected through HTML taken from page 1: answer = no
+    # Now collect the data from REV
+    titleList = []
+    serialList = []
 
-print(content)
+    for url in urls:
+        # Making a GET request
+        r = requests.get(url)     # later extend to loop through all page URLs
+
+        # check status code for response received
+        # success code - 200
+        #print(r)
+
+        # Parsing the HTML
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        # Get this info from inspecting the webpage
+        page = soup.find('ul', class_='productGrid')          # ul class productGrid    
+        #print(s)
+        titles0 = page.find_all('h3', class_='card-title')       # h3 card-title
+        serials0= page.find_all('div', class_="card-text card-text--sku")     # div card-text card-text--sku
+
+        # Stripping out the serials
+        serials = extract_content(serials0)
+        serialList += serials
+
+        # Stripping out the titles
+        titles = extract_content(titles0)
+        titleList += titles
+        
+    return titleList, serialList
+
+# Testing REV
+revTitles, revSerials = get_REV_products()
+print(revTitles)
+print(revSerials)
+
+# Testing GoBilda
