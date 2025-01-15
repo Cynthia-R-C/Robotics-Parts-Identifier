07796f3f5bed1e7519c928b5134dfccc12d7e612
Cynthia C <119439581+Cynthia-R-C@users.noreply.github.com>
2025-01-14 21:30:53 -0600

    Update step_name_scraper.py
    See 1/14 progress log for details

--- src/step_name_scraper.py
+++ src/step_name_scraper.py
@@ -44,6 +44,21 @@ def extract_content(things):
     #     newStrs.append(remove_angle_brackets(str))
     # return newStrs
 
+def make_url_format(strs):
+    '''list of strings --> list of strings
+    Reformats each string in the list into its URL format - used for get_GoBilda_products()'''
+    newStrs = []
+    for str in strs:
+        strLow = str.lower()    # change to lowercase
+        # change special characters to "-"
+        if "&" in str:
+            newStr = strLow.replace(" & ","-")
+        elif " " in str:
+            newStr = strLow.replace(" ","-")
+        else:
+            newStr = strLow
+        newStrs.append(newStr)
+    return newStrs
 
 def get_REV_products():
     '''nothing --> list titles, list serial numbers
@@ -86,9 +101,45 @@ def get_REV_products():
         
     return titleList, serialList
 
+def get_GoBilda_products():
+    '''nothing --> list titles, list serial numbers
+    Returns a list of product names and a list of product serial numbers of all products from GoBilda'''
+    mainCategories = ["https://www.gobilda.com/structure",
+                      "https://www.gobilda.com/motion",
+                      "https://www.gobilda.com/electronics"
+                      "https://www.gobilda.com/hardware",
+                      "https://www.gobilda.com/kits",
+                      "https://www.gobilda.com/merch"]
+    url = "https://www.gobilda.com/structure"           # the URL I'm using to run tests right now
+    
+    # Making a GET request
+    r = requests.get(url)     # later extend to loop through all page URLs
+
+    # check status code for response received
+    # success code - 200
+    #print(r)
+
+    # Parsing the HTML
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # Get this info from inspecting the webpage
+    page = soup.find('ul', class_='productGrid')          # ul class productGrid    
+    #print(s)
+    subcategories = extract_content(page.find_all('li', class_='product'))       # Extracts list of strings of subcategory names in this category
+    #print(subcategories)
+    subcatSegments = make_url_format(subcategories)
+    #print(subcatSegments)
+    
+    #for seg in subcatSegments:   # for each of the subcategory URL segments (get back to this later)
+    exSubURL = "https://www.gobilda.com/channel"    # try to extract product name and serial number using this example first
+
 # Testing REV
 revTitles, revSerials = get_REV_products()
-print(revTitles)
-print(revSerials)
+#print(revTitles)
+#print(revSerials)
 
 # Testing GoBilda
+# bildaTitles, bildaSerials = 
+get_GoBilda_products()
+# print(bildaTitles)
+# print(bildaSerials)
